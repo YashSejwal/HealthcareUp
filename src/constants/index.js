f36fd7e5b9ef8b1b587f5723e5e7672302451976
YASH SEJWAL <yashksejwal13@gmail.com>
2023-05-22 19:15:12 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,200 @@
+import { people01, people02, people03, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star, } from "../assets";
+
+import github from "../assets/github.svg";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Appointments",
+  },
+  {
+    id: "clients",
+    title: "Medicines",
+  },
+  {
+    id: "clients",
+    title: "Login",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Rewards",
+    content:
+      "The best  offers, on medicines and appointments, some tantalizing combinations of promotions and prizes",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "100% Secured User Information",
+    content:
+      "We take proactive steps make sure your information, medical history and transactions are secure.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Balance Transfer",
+    content:
+      "A balance transfer credits can save you a lot of money in buying medicines and booking doctor appointments.",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "The next-generation platform which serves all your health needs.                   #bestInClass",
+    name: "Deepanshu Sharma",
+    title: "HIV & Aids | Patient",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Healthcare Up makes your life easier. If you're lucky to have it, you're lucky.",
+    name: "Vishal Maurya",
+    title: "Polio | Patient",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "The name really goes with the service. It's a great platform which in real sense makes your health care up.",
+    name: "Dr. Sahil Verma",
+    title: "Cancer Specialist | Patient",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Active Users",
+    value: "800+",
+  },
+  {
+    id: "stats-2",
+    title: "Trusted by Hospitals",
+    value: "230+",
+  },
+  {
+    id: "stats-3",
+    title: "Revenue Generated",
+    value: "₹23M+",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://www.hoobank.com/content/",
+      },
+      {
+        name: "How it Works",
+        link: "https://www.hoobank.com/how-it-works/",
+      },
+      {
+        name: "Create",
+        link: "https://www.hoobank.com/create/",
+      },
+      {
+        name: "Explore",
+        link: "https://www.hoobank.com/explore/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.hoobank.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.hoobank.com/help-center/",
+      },
+      {
+        name: "Partners",
+        link: "https://www.hoobank.com/partners/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.hoobank.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.hoobank.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.hoobank.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://www.hoobank.com/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.hoobank.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/yash._.sejwal/",
+  },
+  {
+    id: "social-media-2",
+    icon: linkedin,
+    link: "https://www.linkedin.com/in/yash-sejwal-93717921a/",
+  },
+  {
+    id: "social-media-3",
+    icon: github,
+    link: "https://github.com/YashSejwal",
+  },
+
+  
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
